097ac490b397bf9231baa6d5de2f3cd5b736d88b
zhadan93 <zhadan.evgenya@yandex.ru>
2022-02-07 21:54:50 +0300

    feat: add audio change

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -3,6 +3,9 @@ const birdNav = document.querySelectorAll('.nav__item');
 const logo = document.querySelector('.logo');
 const main = document.querySelector('.main');
 
+let isPlay = false;
+let activeBird = 'forest';
+
 function changeActive(activeElement, className) {
   birdNav.forEach(item => item.classList.remove('nav__item_active'));
   logo.classList.remove('logo_active');
@@ -11,14 +14,20 @@ function changeActive(activeElement, className) {
 
 birdContainer.addEventListener('click', event => {
   if (event.target.classList.contains('nav__item')) { 
+    activeBird = event.target.dataset.bird;
     changeActive(event.target, 'nav__item');
-    changeBirdImage(event.target.dataset.bird);
+    changeBirdImage(activeBird);
+    isPlay = true;
+    changeAudio();
   }
 });
 
 logo.addEventListener('click', () => {
+  activeBird = logo.dataset.bird;
   changeActive(logo, 'logo');
-  changeBirdImage(logo.dataset.bird);
+  changeBirdImage(activeBird);
+  isPlay = true;
+  changeAudio();
 });
 
 
@@ -35,3 +44,31 @@ preloadBirdImages();
 function changeBirdImage(bird) {
   main.style.backgroundImage = `url('./assets/img/${bird}.jpg')`;
 }
+
+const playButton = document.querySelector('.play');
+const playButtonIcon = document.querySelector('.play__btn-icon');
+const audio = new Audio();
+
+function togglePlayAudio() {
+  audio.src = `./assets/audio/${activeBird}.mp3`;
+  audio.currentTime = 0; 
+
+  isPlay ? audio.play() : audio.pause();
+}
+
+function changePlayButton() {
+  const playIcon = isPlay ? 'pause' : 'play';
+
+  playButtonIcon.setAttribute('href', `./assets/svg/sprite.svg#${playIcon}`);
+}
+
+function changeAudio() {
+  changePlayButton();
+  togglePlayAudio();
+}
+
+playButton.addEventListener('click', () => {
+  isPlay = isPlay ? false : true;
+
+  changeAudio();
+})
