12a758ef748175130ee51a6effce61720e21ebe0
zhadan93 <zhadan.evgenya@yandex.ru>
2022-02-21 20:07:33 +0300

    feat: implement button restart

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -2,36 +2,84 @@ const gameField = document.querySelector('.game-field');
 const restartButton = document.querySelector('.button-restart');
 const playButton = document.querySelector('.button-play');
 
-function drawGameField() {
-  gameField.classList.remove('hide');
-  restartButton.classList.remove('hide');
-}
-
 function hidePlayButton() {
   playButton.classList.add('hide');
 } 
 
-function showPlayingField() {
+function drawGameField() {
   hidePlayButton();
-  drawGameField();
+  gameField.classList.remove('hide');
+  restartButton.classList.remove('hide');
 }
 
-playButton.addEventListener('click', showPlayingField);
+playButton.addEventListener('click', drawGameField);
 
 
-let figure = 'X';
-let clickedCells = [];
+let gameSteps = 0;
+let currentPlayer = 'X';
+let playedCells = [];
 
-function drawFigure(event) {
-  const {cellIndex} = event.dataset;
+function drawFigure(cellPlayed) {
+  cellPlayed.textContent = currentPlayer == 'X' ? currentPlayer : 'O';
+}
 
-  if (clickedCells[cellIndex] !== undefined) return; 
+function changeGameStates(cellIndex) {
+  playedCells[cellIndex] = currentPlayer;
+  currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+  gameSteps++;
+}
 
-  clickedCells[cellIndex] = figure;
-  event.textContent = figure == 'X' ? figure : 'O';
-  figure = figure == 'X' ? 'O' : 'X';
+function handleCellClick(cellClick) {
+  const {cellIndex} = cellClick.dataset;
+
+  if (playedCells[cellIndex] !== undefined) return;
+  
+  drawFigure(cellClick);
+  changeGameStates(cellIndex);
 }
 
 gameField.addEventListener('click', event => {
-  if (event.target.classList.contains('cell')) drawFigure(event.target);
-})
+  if (event.target.classList.contains('cell')) handleCellClick(event.target);
+});
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+/*function getResult() {
+  for (let i = 0; i < 8; i++) {
+    const winCombination = winningCombinations[i];
+    let a = clickedCells[winCombination[0]];
+    let b = clickedCells[winCombination[1]];
+    let c = clickedCells[winCombination[2]];
+    if (a === undefined || b === undefined || c === undefined) {
+        continue;
+    }
+    if (a === b && b === c) {
+      return {
+        win: winCombination,
+        figure: a,
+      }
+    }
+  }
+}*/
+
+const cells = document.querySelectorAll('.cell');
+
+function resetGame() {
+  gameSteps = 0;
+  currentPlayer = 'X';
+
+  cells.forEach(cell => cell.textContent = '');
+
+  playedCells = [];
+}
+
+restartButton.addEventListener('click', resetGame);
